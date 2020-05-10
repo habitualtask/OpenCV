925204d7514a556b33c80f3c2941c2dfa043d30b
song2 <msms9957@gmail.com>
2020-05-11 01:25:01 +0900

    2020-05-11

--- video_ALU.py
+++ video_ALU.py
@@ -8,15 +8,15 @@ plus = original+a
 minus = original-a
 divide = original/a
 mult = original*a
-AND = original&a
-XOR = original^a
+AND = cv2.bitwise_and(original,a)
+XOR = cv2.bitwise_xor(original,a)
 
+cv2.imshow("original",original)
 cv2.imshow("+", plus)
 cv2.imshow("-", minus)
 cv2.imshow("/", divide)
 cv2.imshow("*", mult)
 cv2.imshow("AND", AND)
 cv2.imshow("XOR",XOR)
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
