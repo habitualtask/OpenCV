47bb4bcda719e99911691924f408f5b688bae31d
song2 <msms9957@gmail.com>
2020-05-13 23:09:15 +0900

    2020-05-13

---
+++ video_Blurring.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+image = cv2.imread("Lenna.png")
+
+rows, cols = image.shape[:2]
+
+low_pass_filter_3x3 = np.ones((3, 3), np.float32) / 9.0
+low_pass_filter_5x5 = np.ones((5, 5), np.float32) / 25.0
+low_pass_filter_7x7 = np.ones((7, 7), np.float32) / 49.0
+
+cv2.imshow('Source', image)
+
+dst = cv2.filter2D(image, -1, low_pass_filter_3x3)
+cv2.imshow('low_pass_filter_3x3', dst)
+
+dst = cv2.filter2D(image, -1, low_pass_filter_5x5)
+cv2.imshow('low_pass_filter_5x5', dst)
+
+dst = cv2.filter2D(image, -1, low_pass_filter_7x7)
+cv2.imshow('low_pass_filter_7x7', dst)
+
+cv2.waitKey()
